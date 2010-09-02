45fea591e7aeb754e9e182ad843ebfaac01f9c4f
Nat Weerawan <nat.wrw@gmail.com>
2010-09-02 13:29:47 +0700

    Implement Decode action

--- grails-app/controllers/zebra/v2/UploadController.groovy
+++ grails-app/controllers/zebra/v2/UploadController.groovy
@@ -1,13 +1,16 @@
 package zebra.v2
 
 import com.google.zxing.DecodeHintType;
-import com.google.zxing.MonochromeBitmapSource;
+import com.google.zxing.BinaryBitmap;
+//import com.google.zxing.MonochromeBitmapSource;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.ReaderException;
 import com.google.zxing.Result;
 import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.ResultParser;
-
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -25,11 +28,18 @@ class UploadController {
     def index = { render 'Hello, Grails' }
 	def upload = {
 		def file = request.getFile('myFile')
-		if(file && !file.empty && file.size < 1000000){
+		if(file && !file.empty){
 			//file.transferTo( new File("ปลายทาง" )
-			render 'Ok' 
-			render '<br>'
-			render file.getOriginalFilename() 
+			def image = ImageIO.read(file.getInputStream())	
+			LuminanceSource source = new BufferedImageLuminanceSource(image);
+
+			BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+			Result result = new MultiFormatReader().decode(bitmap);
+
+			ParsedResult parsedResult = ResultParser.parseResult(result);
+			render(file.getOriginalFilename() + " (format: " + result.getBarcodeFormat() +
+			  ", type: " + parsedResult.getType() + "):<br>\nRaw result: " + result.getText() +
+			 "<br>\nParsed result: " + parsedResult.getDisplayResult());
 		}
 		else {
 			render 'Error' 
