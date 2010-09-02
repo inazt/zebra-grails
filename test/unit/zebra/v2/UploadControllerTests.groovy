d6a236d9f5e0dc04d0d8432e97244bb4ccdd6b24
Nat Weerawan <nat.wrw@gmail.com>
2010-09-02 12:14:26 +0700

    Track test directory

---
+++ test/unit/zebra/v2/UploadControllerTests.groovy
@@ -0,0 +1,17 @@
+package zebra.v2
+
+import grails.test.*
+
+class UploadControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
