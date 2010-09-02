15f57b170b7cc1361b9edf4f27ea98bdffc24349
Nat Weerawan <nat.wrw@gmail.com>
2010-09-02 12:12:46 +0700

    Handle upload action

--- grails-app/controllers/zebra/v2/UploadController.groovy
+++ grails-app/controllers/zebra/v2/UploadController.groovy
@@ -2,5 +2,30 @@ package zebra.v2
 
 class UploadController {
 
-    def index = { }
+    def index = { render 'Hello, Grails' }
+	def upload = {
+		def file = request.getFile('myFile')
+		if(file && !file.empty && file.size < 1000000){
+			//file.transferTo( new File("ปลายทาง" )
+			render 'Ok' 
+			render '<br>'
+			render file.getOriginalFilename() 
+		}
+		else {
+			render 'Error' 
+		} 
+	}
 }
+/**
+*
+	interface MultipartFile {
+	public byte[] getBytes();
+	public String getContentType();
+	public java.io.InputStream getInputStream();
+	public String getName();
+	public String getOriginalFilename();
+	public long getSize();
+	public boolean isEmpty();
+	public void transferTo(java.io.File dest);
+	}
+*/
